26de7a38ab2f30fd51329f6c9a8a54b503c4e59b
Himanshu Sharma <sdothimanshu@gmail.com>
2020-12-13 19:24:33 +0530

    created first route

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,10 +1,9 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable_card.dart';
 import 'reusable_gender.dart';
 import 'constants.dart';
+import 'reusable_bottom_card.dart';
 
 const Color activecardcolor = Color(0xFF686d76);
 const Color inactivecardcolor = Color(0xFF393e46);
@@ -140,84 +139,17 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(
-                    cardchild: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'WEIGHT',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.grey.shade300,
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.baseline,
-                          children: [
-                            Text(
-                              '$weight',
-                              style: kstyle,
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(left: 5),
-                              child: Text(
-                                'kg',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.grey.shade300,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            GestureDetector(
-                                onLongPress :(){
-                                  setState(() {
-                                    weight+=10;
-                                  });
-                                },
-                                child: FloatingActionButton(
-                                onPressed: () {
-                                  setState(() {
-                                    weight++;
-                                  });
-                                },
-                                backgroundColor: inactivecardcolor,
-                                child: Icon(Icons.add),
-                              ),
-                            ),
-                            GestureDetector(
-                                
-                                onLongPress: (){
-                                  setState(() {
-                                    weight=weight-10;
-                                  });
-                                },
-                                child: FloatingActionButton(
-                                backgroundColor: inactivecardcolor,
-                                onPressed: () {
-                                  setState(() {
-                                    weight--;
-                                  });
-                                },
-                                child: Icon(Icons.remove),
-                                
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                    colour: activecardcolor,
+                  child: ReusableBottomCard(
+                    input: weight,
+                    type: 'WEIGHT',
+                    weightType: 'Kg',
                   ),
                 ),
                 Expanded(
-                  child: ReusableCard(
-                    colour: activecardcolor,
+                  child: ReusableBottomCard(
+                    input: age,
+                    type: 'AGE',
+                    weightType: 'Yrs',
                   ),
                 ),
               ],
@@ -228,6 +160,15 @@ class _InputPageState extends State<InputPage> {
               borderRadius: BorderRadius.circular(10),
               color: Color(0xFFEB1555),
             ),
+            child: Center(
+                child: Text(
+              'CALCULATE',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 3,
+              ),
+            )),
             height: 80,
             width: double.infinity,
           )
--- lib/reusable_bottom_card.dart
+++ lib/reusable_bottom_card.dart
@@ -1,8 +1,101 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
+import 'constants.dart';
 
-class ReusableBottomCard extends StatefulWidget{
+class ReusableBottomCard extends StatefulWidget {
+  final int input;
+  final String type,weightType;
 
+  ReusableBottomCard({@required this.input,@required this.type,@required this.weightType});
 
- 
+  @override
+  _ReusableBottomCardState createState() =>
+      _ReusableBottomCardState(input,type,weightType);
+}
 
+class _ReusableBottomCardState extends State<ReusableBottomCard> {
+  int input;
+  String type,weightType;
+
+  _ReusableBottomCardState(this.input,this.type,this.weightType);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            type,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.grey.shade300,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            children: [
+              Text(
+                '$input',
+                style: kstyle,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 5),
+                child: Text(
+                  weightType,
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.grey.shade300,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onLongPress: () {
+                  setState(() {
+                    input += 10;
+                  });
+                },
+                child: FloatingActionButton(
+                  onPressed: () {
+                    setState(() {
+                      input++;
+                    });
+                  },
+                  backgroundColor: inactivecardcolor,
+                  child: Icon(Icons.add),
+                ),
+              ),
+              GestureDetector(
+                onLongPress: () {
+                  setState(() {
+                    input -= 10;
+                  });
+                },
+                child: FloatingActionButton(
+                  backgroundColor: inactivecardcolor,
+                  onPressed: () {
+                    setState(() {
+                      input--;
+                    });
+                  },
+                  child: Icon(Icons.remove),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+      margin: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Color(0xFF686d76),
+      ),
+    );
+  }
 }
